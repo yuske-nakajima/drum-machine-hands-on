a3784a91ddf506ac6fc24b688731899eb61634f1
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-10-20 15:05:07 +0900

    アプリで使用する色を設定

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+// 定数
+const DM_WIDTH = 800
+const DM_HEIGHT = 530
+const DM_COLORS = {}
---
+++ src/initial.js
@@ -0,0 +1,23 @@
+function initial() {
+  createCanvas(DM_WIDTH, DM_HEIGHT)
+  colorMode(HSB)
+
+  // 色の定義
+  DM_COLORS.machineDark = color(220, 10, 50, 1)
+  DM_COLORS.machineLight = color(220, 10, 70, 1)
+  DM_COLORS.machineLine = color(220, 50, 30, 1)
+  DM_COLORS.machineText = color(220, 10, 100, 1)
+
+  DM_COLORS.designGuide = color(305, 100, 100, 1)
+
+  DM_COLORS.seqMain = color(220, 10, 50, 1)
+  DM_COLORS.seqAccent = color(220, 10, 10, 1)
+
+  DM_COLORS.buttonNormal = color(50, 10, 100, 1)
+  DM_COLORS.buttonAccent = color(0, 50, 100, 1)
+  DM_COLORS.buttonLine = color(50, 25, 100, 1)
+  DM_COLORS.buttonText = color(0, 0, 30, 1)
+
+  DM_COLORS.displayMain = color(120, 90, 70, 1)
+  DM_COLORS.displayText = color(120, 70, 95, 1)
+}
