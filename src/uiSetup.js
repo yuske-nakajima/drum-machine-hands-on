64bb290ea24948ddd7cff9102a0514a2ba0072c9
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-10-23 23:34:56 +0900

    ネストの深い箇所を調整

--- src/uiSetup.js
+++ src/uiSetup.js
@@ -1,85 +1,83 @@
 function dmUiSetup() {
+  // ドラム・マシンの背景
   dmDrawBlock(() => {
+    fill(DM_COLORS.machineDark)
+
+    // 画面の高さ・幅で四角形を描画する際に線の太さの半分が画面外に出るため
+    // 考慮して線の太さを2倍にしている
+    strokeWeight(DM_LINE_WEIGHT * 2)
     stroke(DM_COLORS.machineLine)
 
-    // ドラム・マシンの背景
-    dmDrawBlock(() => {
-      fill(DM_COLORS.machineDark)
+    rect(0, 0, DM_WIDTH, DM_HEIGHT)
+  })
 
-      // 画面の高さ・幅で四角形を描画する際に線の太さの半分が画面外に出るため
-      // 考慮して線の太さを2倍にしている
-      strokeWeight(DM_LINE_WEIGHT * 2)
+  // ドラム・マシンの操作部分の背景
+  dmDrawBlock(() => {
+    fill(DM_COLORS.machineLight)
+    strokeWeight(DM_LINE_WEIGHT)
+    stroke(DM_COLORS.machineLine)
 
-      rect(0, 0, DM_WIDTH, DM_HEIGHT)
-    })
-
-    // ドラム・マシンの操作部分の背景
-    dmDrawBlock(() => {
-      fill(DM_COLORS.machineLight)
-      strokeWeight(DM_LINE_WEIGHT)
-
-      // volume・tempoエリア controlエリア patternエリアの描画
-      for (let i = 0; i < 3; i++) {
-        // ボタンの描画エリア
-        rect(
-          DM_POSITIONS.controlArea.x + i * (DM_PARTS_SIZES.controlArea.width + DM_AREA_GAP),
-          DM_POSITIONS.controlArea.y,
-          DM_PARTS_SIZES.controlArea.width,
-          DM_PARTS_SIZES.controlArea.height,
-        )
-      }
-
-      // メインエリアの描画
+    // volume・tempoエリア controlエリア patternエリアの描画
+    for (let i = 0; i < 3; i++) {
+      // ボタンの描画エリア
       rect(
-        DM_POSITIONS.mainArea.x,
-        DM_POSITIONS.mainArea.y,
-        DM_PARTS_SIZES.mainArea.width,
-        DM_PARTS_SIZES.mainArea.height,
+        DM_POSITIONS.controlArea.x + i * (DM_PARTS_SIZES.controlArea.width + DM_AREA_GAP),
+        DM_POSITIONS.controlArea.y,
+        DM_PARTS_SIZES.controlArea.width,
+        DM_PARTS_SIZES.controlArea.height,
       )
-    })
+    }
 
-    // シーケンスライトの背景
-    dmDrawBlock(() => {
-      fill(DM_COLORS.seqMain)
-      stroke(DM_COLORS.machineLine)
-      strokeWeight(DM_LINE_WEIGHT)
+    // メインエリアの描画
+    rect(
+      DM_POSITIONS.mainArea.x,
+      DM_POSITIONS.mainArea.y,
+      DM_PARTS_SIZES.mainArea.width,
+      DM_PARTS_SIZES.mainArea.height,
+    )
+  })
 
-      ellipseMode(CENTER)
-      for (let i = 0; i < DM_BEAT; i++) {
-        // 4拍ごとにアクセントをつける
-        const isAccent = i % 4 === 0
-        strokeWeight(DM_LINE_WEIGHT * (isAccent ? 2 : 1))
+  // シーケンスライトの背景
+  dmDrawBlock(() => {
+    fill(DM_COLORS.seqMain)
+    stroke(DM_COLORS.machineLine)
+    strokeWeight(DM_LINE_WEIGHT)
 
-        ellipse(
-          DM_POSITIONS.seqLight.x + i * DM_PARTS_SIZES.seqCell.width,
-          DM_POSITIONS.seqLight.y,
-          DM_PARTS_SIZES.seqCell.width / 2,
-          DM_PARTS_SIZES.seqCell.height / 2,
-        )
-      }
-    })
+    ellipseMode(CENTER)
+    for (let i = 0; i < DM_BEAT; i++) {
+      // 4拍ごとにアクセントをつける
+      const isAccent = i % 4 === 0
+      strokeWeight(DM_LINE_WEIGHT * (isAccent ? 2 : 1))
 
-    // テキスト
-    dmDrawBlock(() => {
-      fill(DM_COLORS.machineText)
-      noStroke()
-      textSize(DM_PARTS_SIZES.seqCell.height / 4)
-      textStyle(BOLD)
-      textAlign(CENTER, CENTER)
+      ellipse(
+        DM_POSITIONS.seqLight.x + i * DM_PARTS_SIZES.seqCell.width,
+        DM_POSITIONS.seqLight.y,
+        DM_PARTS_SIZES.seqCell.width / 2,
+        DM_PARTS_SIZES.seqCell.height / 2,
+      )
+    }
+  })
 
-      // Volume
-      text('VOLUME', DM_POSITIONS.volumeText.x, DM_POSITIONS.volumeText.y)
+  // テキスト
+  dmDrawBlock(() => {
+    fill(DM_COLORS.machineText)
+    noStroke()
+    textSize(DM_PARTS_SIZES.seqCell.height / 4)
+    textStyle(BOLD)
+    textAlign(CENTER, CENTER)
 
-      // Tempo
-      text('TEMPO', DM_POSITIONS.tempoText.x, DM_POSITIONS.tempoText.y)
+    // Volume
+    text('VOLUME', DM_POSITIONS.volumeText.x, DM_POSITIONS.volumeText.y)
 
-      // Pattern
-      text('PATTERN', DM_POSITIONS.patternText.x, DM_POSITIONS.patternText.y)
+    // Tempo
+    text('TEMPO', DM_POSITIONS.tempoText.x, DM_POSITIONS.tempoText.y)
 
-      // 音色
-      for (let i = 0; i < DM_MUSIC_LIST.length; i++) {
-        text(DM_MUSIC_LIST[i].name, DM_POSITIONS.seqText.x, DM_POSITIONS.seqText.y + i * DM_PARTS_SIZES.seqCell.height)
-      }
-    })
+    // Pattern
+    text('PATTERN', DM_POSITIONS.patternText.x, DM_POSITIONS.patternText.y)
+
+    // 音色
+    for (let i = 0; i < DM_MUSIC_LIST.length; i++) {
+      text(DM_MUSIC_LIST[i].name, DM_POSITIONS.seqText.x, DM_POSITIONS.seqText.y + i * DM_PARTS_SIZES.seqCell.height)
+    }
   })
 }
